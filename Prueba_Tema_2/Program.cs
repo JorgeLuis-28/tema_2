44a312f2e173f52c362554d5c723571aea35d042
juleysi <juleysichomba@gmail.com>
2026-09-04 19:02:03 -0500

    version 2

---
+++ Prueba_Tema_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Tema_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la Nota 1(T1): ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int N1 = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Write("Ingrese la Nota 2(T2): ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int N2 = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Write("Ingrese la Nota 3(T3): ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int N3 = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Write("Ingrese la Nota 4(EP): ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int N4 = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Write("Ingrese la Nota 5(EF): ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int N5 = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            float Total;
+            Total = (float)((N1 * 0.10) + (N2 * 0.10) + (N3 * 0.10) + (N4 * 0.20) + (N5 * 0.50));
+            Console.Write("La nota final es: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Total);
+            Console.ResetColor();
+
+            Console.Write("Calculando estado.... ");
+        }
+    }
+}
